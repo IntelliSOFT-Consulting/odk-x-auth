fe6535599306ac9e01dcf246bc8e2dbd676f6dc6
Amolo <railamolo@gmail.com>
2022-10-04 09:13:03 +0000

    fix registration error

--- api/app/lib/email.py
+++ api/app/lib/email.py
@@ -1,45 +1,8 @@
 import smtplib
 import ssl
-
 from app.config import SMTP_HOST, SMTP_PASSWORD, SMTP_PORT, SMTP_SENDER, SMTP_SSL
 
-port = 465  # For SSL
-password = input("Type your password and press enter: ")
-
-# Create a secure SSL context
 context = ssl.create_default_context()
-
-# with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-#     server.login("my@gmail.com", password)
-#     # TODO: Send email here
-
-
-def send_reset_link(email):
-
-    res = send_email()
-
-
-def reset_password(email, reset_code):
-    pass
-
-
-context = ssl.create_default_context()
-
-
-def send_email(recepient, subject, email):
-
-    try:
-        server = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
-        server.ehlo()
-        if SMTP_SSL:
-            server.starttls(context=context)
-        server.login(SMTP_SENDER, SMTP_PASSWORD)
-        server.sendmail(SMTP_SENDER, recepient, email)
-
-        return {"status": "success"}
-    except Exception as e:
-        return {"status": "error", "error": e}
-
 
 welcome_message = """\
 Subject: ODK-X Auth - New Account
@@ -67,3 +30,21 @@ Click on the link below to reset your password and get started.
 
 - ODK-X Auth Admin
 """
+
+
+def send_email(recepient, email_type="reset"):
+
+    try:
+        server = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+        server.ehlo()
+        if SMTP_SSL:
+            server.starttls(context=context)
+        server.login(SMTP_SENDER, SMTP_PASSWORD)
+        if email_type == "reset":
+            email = reset_password_message.format(recepient, recepient)
+        elif email_type == "welcome":
+            email = welcome_message.format(recepient, recepient)
+        server.sendmail(SMTP_SENDER, recepient, email)
+        return {"status": "success"}
+    except Exception as e:
+        return {"status": "error", "error": e}
--- api/app/routes/auth.py
+++ api/app/routes/auth.py
@@ -1,40 +1,46 @@
 from flask import Blueprint, request, jsonify
+from app.lib.email import send_email
 
 from app.lib.auth import add_new_user, generate_token, ldap_client
 
-bp = Blueprint('auth', __name__, url_prefix='/api/auth')
+bp = Blueprint("auth", __name__, url_prefix="/api/auth")
 
 
-@bp.route('/login', methods=['POST'])
+@bp.route("/login", methods=["POST"])
 def login():
     try:
         data = request.get_json()
-        client = ldap_client(user=data['user'], password=data['password'])
+        client = ldap_client(user=data["user"], password=data["password"])
         print(client)
         if client:
-            return jsonify(status="success", token=generate_token(data['user'])), 200
+            return jsonify(status="success", token=generate_token(data["user"])), 200
         return jsonify(error="Invalid username / password", status="error"), 401
     except Exception as e:
         return jsonify(error=str(e), status="error"), 401
 
 
-@bp.route('/register', methods=['POST'])
+@bp.route("/register", methods=["POST"])
 def register():
     try:
         data = request.get_json()
-        response = add_new_user(data['first_name'], data['last_name'] , data['email'], data['gidNumber'])
-        return jsonify(response), 200 if response['status'] == "success" else 400
+        response = add_new_user(
+            data["first_name"], data["last_name"], data["email"], data["gidNumber"]
+        )
+        print(response)
+        if response["status"] == "success":
+            send_email(data["email"], email_type="welcome")
+        return jsonify(response), 200 if response["status"] == "success" else 400
     except Exception as e:
         return jsonify(error=str(e), status="error"), 400
 
 
-@bp.route('/reset-password', methods=['GET', 'POST'])
+@bp.route("/reset-password", methods=["GET", "POST"])
 def update_password():
-    if request.method == 'GET':
-        token = request.args.get('token')
-        user = request.args.get('user')
+    if request.method == "GET":
+        token = request.args.get("token")
+        user = request.args.get("user")
         return jsonify(status="success", message="Password reset successfully")
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
-        password = data['password']
+        password = data["password"]
         return jsonify(status="success", message="Password reset instructions sent")
