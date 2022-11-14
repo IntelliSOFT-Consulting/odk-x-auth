2ea964b4da0d7196393f8637cdd37a24808d5f52
Amolo <railamolo@gmail.com>
2022-11-14 23:41:49 +0300

    support setting default passwords, editing user details, editing groups

--- api/app/routes/users.py
+++ api/app/routes/users.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from app.lib.auth import admin_token_required
+from flask import Blueprint, jsonify, request
+from app.lib.auth import admin_token_required, modify_password, modify_user_group
 from app.lib.groups import delete_ldap_user
 from app.lib.search import search_ldap
 
@@ -27,7 +27,11 @@ def get_user(name):
 @admin_token_required
 def edit_user(name):
     try:
-        response = delete_ldap_user(name)
+        data = request.get_json()
+        if data['password']:
+            response = modify_password(name, data['password'])
+        if data['gidNumber']:
+            response = modify_user_group(name, data["gidNumber"])
         return jsonify(response), 200 if response['status'] == "success" else 400
     except Exception as e:
         return jsonify(error=str(e), status="error"), 400
